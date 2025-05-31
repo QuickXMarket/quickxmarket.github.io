f783e1101182fee02fae0840b6463401c8b32b5d
MudiaZuwa <94544783+MudiaZuwa@users.noreply.github.com>
2025-05-31 09:15:34 +0100

    paystack webhook

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authUser from '../middlewares/authUser.js';
-import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderPaystack } from '../controllers/orderController.js';
+import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderPaystack, paystackWebhooks } from '../controllers/orderController.js';
 import authSeller from '../middlewares/authSeller.js';
 
 const orderRouter = express.Router();
@@ -9,5 +9,8 @@ orderRouter.post('/cod', authUser, placeOrderCOD)
 orderRouter.get('/user', authUser, getUserOrders)
 orderRouter.get('/seller', authSeller, getAllOrders)
 orderRouter.post('/paystack', authUser, placeOrderPaystack)
+
+// Add webhook route for Paystack payment verification (no auth)
+orderRouter.post('/paystack-webhook', paystackWebhooks)
 
 export default orderRouter;
--- server/server.js
+++ server/server.js
@@ -35,8 +35,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      callback(null, origin || "*");
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
     },
     credentials: true,
   })
